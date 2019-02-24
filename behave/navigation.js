375cc3118b244fabb0bbf8f5af8f4d8187857f91
Chris <christian@Andrea-Borcherss-iMac.local>
2019-02-24 11:38:50 +0100

    beautifiy

--- behave/navigation.js
+++ behave/navigation.js
@@ -1,24 +1,22 @@
-
 var nav_aube = document.getElementById("nav_aube");
-nav_aube.addEventListener("click",function(){
+nav_aube.addEventListener("click", function () {
     versteckeAlle();
-    document.getElementById("berechne").classList.replace('sech','secs');
+    document.getElementById("berechne").classList.replace('sech', 'secs');
 });
 
 var nav_about = document.getElementById("nav_about");
-nav_about.addEventListener("click",function(){
+nav_about.addEventListener("click", function () {
     versteckeAlle();
-    document.getElementById("about").classList.replace('sech','secs');
+    document.getElementById("about").classList.replace('sech', 'secs');
 });
 
 
 
-function versteckeAlle(){
+function versteckeAlle() {
     var hide = document.getElementsByClassName('secs');
-    if(hide.length > 0){
-    for(var i = 0; i<= hide.length; i++){
-        hide[i].classList.replace('secs','sech');
+    if (hide.length > 0) {
+        for (var i = 0; i <= hide.length; i++) {
+            hide[i].classList.replace('secs', 'sech');
+        }
     }
 }
-}
-
