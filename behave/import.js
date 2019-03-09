8aa1b164ebc7d892d50db0305594a43eb19269cf
Chris <christian@Andrea-Borcherss-iMac.local>
2019-03-09 18:27:28 +0100

    .

--- behave/import.js
+++ behave/import.js
@@ -1,10 +1,10 @@
-const links = document.querySelectorAll('link[rel="import"]')
+const links = document.querySelectorAll('link[rel="import"]');
 
 // Import and add each page to the DOM
 Array.prototype.forEach.call(links, (link) => {
   let template = link.import.querySelector('.template')
-  let clone = document.importNode(template.content, true)
+  let clone = document.importNode(template.content, true);
  
-    document.querySelector('.content').appendChild(clone)
+    document.querySelector('.content').appendChild(clone);
   
-})
+});
